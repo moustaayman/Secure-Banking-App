8c321a980a1a68f4c12e7f2b146221cf91d70fb1
mousta <moustaayman03@gmail.com>
2024-10-20 14:21:50 +0100

    enhanced the logic of the user registration

--- src/main/java/com/ayman/bankapp/bankingapplication/services/ServiceImpl/UserServiceImpl.java
+++ src/main/java/com/ayman/bankapp/bankingapplication/services/ServiceImpl/UserServiceImpl.java
@@ -1,45 +1,54 @@
 package com.ayman.bankapp.bankingapplication.services.ServiceImpl;
 
-import com.ayman.bankapp.bankingapplication.dtos.BankResponse;
-import com.ayman.bankapp.bankingapplication.dtos.UserRequest;
+import com.ayman.bankapp.bankingapplication.dtos.UserRegistrationRequest;
+import com.ayman.bankapp.bankingapplication.dtos.UserResponse;
 import com.ayman.bankapp.bankingapplication.entities.User;
-import com.ayman.bankapp.bankingapplication.enums.UserStatus;
+import com.ayman.bankapp.bankingapplication.enums.Role;
+import com.ayman.bankapp.bankingapplication.exceptions.CustomException;
 import com.ayman.bankapp.bankingapplication.repositories.UserRepository;
 import com.ayman.bankapp.bankingapplication.services.UserService;
-import com.ayman.bankapp.bankingapplication.utils.AccountUtils;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class UserServiceImpl implements UserService{
     private UserRepository userRepository;
     @Override
-    public BankResponse createUser(UserRequest userRequest) {
+    public UserResponse registerUser(UserRegistrationRequest userRegistrationRequest) {
+        //
+        log.info("Registering user with email: {}", userRegistrationRequest.email());
         //checking if the user already has an account
-        if(userRepository.existsByEmail(userRequest.email())) {
-            return BankResponse.builder()
-                    .responseCode("ERROR")
-                    .responseMessage("User already exists")
-                    .build();
+        if(userRepository.existsByEmail(userRegistrationRequest.email())) {
+            throw new CustomException.BadRequestException("Email is already in use");
         }
         //creating a user
-        User newUser = User.builder()
-                .firstName(userRequest.firstName())
-                .lastName(userRequest.lastName())
-                .otherName(userRequest.otherName())
-                .gender(userRequest.gender())
-                .address(userRequest.address())
-                .city(userRequest.city())
-                .email(userRequest.email())
-                .phoneNumber(userRequest.phoneNumber())
-                .alternativePhoneNumber(userRequest.alternativePhoneNumber())
-                .status(UserStatus.ACTIVE)
+        User user = User.builder()
+                .firstName(userRegistrationRequest.firstName())
+                .lastName(userRegistrationRequest.lastName())
+                .email(userRegistrationRequest.email())
+                .password(userRegistrationRequest.password())
+                .phoneNumber(userRegistrationRequest.phoneNumber())
+                .address(userRegistrationRequest.address())
+                .city(userRegistrationRequest.city())
+                .gender(userRegistrationRequest.gender())
+                .roles(Set.of(Role.USER))
                 .build();
-        userRepository.save(newUser);
-        return BankResponse.builder()
-                .responseCode("SUCCESS")
-                .responseMessage("Account created successfully")
+        userRepository.save(user);
+        return UserResponse.builder()
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .email(user.getEmail())
+                .roles(user.getRoles())
                 .build();
+    }
+
+    @Override
+    public User getUserByEmail(String email) {
+        return null;
     }
 }
