52b05d9931a1509bd7c1db3132acfae85bc3a727
mousta <moustaayman03@gmail.com>
2024-10-20 14:36:06 +0100

    add method

--- src/main/java/com/ayman/bankapp/bankingapplication/repositories/AccountRepository.java
+++ src/main/java/com/ayman/bankapp/bankingapplication/repositories/AccountRepository.java
@@ -4,4 +4,5 @@ import com.ayman.bankapp.bankingapplication.entities.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    boolean existsByAccountNumber(String accountNumber);
 }
