a1002dfb587176ebc45988af7bf26662277c1b94
mousta <moustaayman03@gmail.com>
2024-10-22 22:00:11 +0100

    minor changes

--- src/main/java/com/ayman/bankapp/bankingapplication/BankingApplication.java
+++ src/main/java/com/ayman/bankapp/bankingapplication/BankingApplication.java
@@ -1,13 +1,7 @@
 package com.ayman.bankapp.bankingapplication;
 
-import com.ayman.bankapp.bankingapplication.entities.User;
-import com.ayman.bankapp.bankingapplication.repositories.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.math.BigDecimal;
 
 @SpringBootApplication
 public class BankingApplication {
