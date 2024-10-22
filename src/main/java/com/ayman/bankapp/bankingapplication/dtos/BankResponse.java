86c498fbb7e83ee8830bf016ddbabdf1e43d0d41
mousta <moustaayman03@gmail.com>
2024-10-22 21:57:41 +0100

    add record

--- src/main/java/com/ayman/bankapp/bankingapplication/dtos/BankResponse.java
+++
@@ -1,6 +0,0 @@
-package com.ayman.bankapp.bankingapplication.dtos;
-
-import lombok.Builder;
-
-@Builder
-public record BankResponse(String responseCode, String responseMessage, AccountInfo accountInfo) {}
