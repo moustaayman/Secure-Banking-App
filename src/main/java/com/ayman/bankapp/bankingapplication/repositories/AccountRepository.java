71c3245fbd244cacd296839bf4d704cd66092e4e
mousta <moustaayman03@gmail.com>
2024-11-02 18:45:43 +0100

    minor change

--- src/main/java/com/ayman/bankapp/bankingapplication/repositories/AccountRepository.java
+++ src/main/java/com/ayman/bankapp/bankingapplication/repositories/AccountRepository.java
@@ -3,6 +3,9 @@ package com.ayman.bankapp.bankingapplication.repositories;
 import com.ayman.bankapp.bankingapplication.entities.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
     boolean existsByAccountNumber(String accountNumber);
+    Account findByAccountNumber(String accountNumber);
 }
