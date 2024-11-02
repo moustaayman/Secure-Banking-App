ab204b9e6c9d720f593d077c466f830f407f8a60
mousta <moustaayman03@gmail.com>
2024-11-02 18:43:24 +0100

    divided accounts to two types: fromAccount and toAccount

--- src/main/java/com/ayman/bankapp/bankingapplication/entities/Account.java
+++ src/main/java/com/ayman/bankapp/bankingapplication/entities/Account.java
@@ -1,10 +1,15 @@
 package com.ayman.bankapp.bankingapplication.entities;
 
+import com.ayman.bankapp.bankingapplication.enums.AccountStatus;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Builder
@@ -18,7 +23,17 @@ public class Account {
     private String accountName;
     private String accountNumber;
     private BigDecimal accountBalance;
+    @Enumerated(EnumType.STRING)
+    private AccountStatus status;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    @OneToMany(mappedBy = "fromAccount", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Transaction> fromTransactions = new ArrayList<>();
+
+    @OneToMany(mappedBy = "toAccount", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Transaction> toTransactions = new ArrayList<>();
 }
