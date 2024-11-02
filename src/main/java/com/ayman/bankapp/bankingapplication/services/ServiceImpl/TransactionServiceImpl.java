3fad11f64895e4497272caf706b9c9687ffef3d2
mousta <moustaayman03@gmail.com>
2024-11-02 18:49:07 +0100

    added the logic of deposit, withdrawal, transfer and getting an account transactions history

---
+++ src/main/java/com/ayman/bankapp/bankingapplication/services/ServiceImpl/TransactionServiceImpl.java
@@ -0,0 +1,174 @@
+package com.ayman.bankapp.bankingapplication.services.ServiceImpl;
+
+import com.ayman.bankapp.bankingapplication.dtos.*;
+import com.ayman.bankapp.bankingapplication.entities.Account;
+import com.ayman.bankapp.bankingapplication.entities.Transaction;
+import com.ayman.bankapp.bankingapplication.enums.TransactionType;
+import com.ayman.bankapp.bankingapplication.exceptions.CustomException;
+import com.ayman.bankapp.bankingapplication.repositories.AccountRepository;
+import com.ayman.bankapp.bankingapplication.repositories.TransactionRepository;
+import com.ayman.bankapp.bankingapplication.services.TransactionService;
+import lombok.AllArgsConstructor;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.slf4j.Logger;
+
+@Service
+@AllArgsConstructor
+public class TransactionServiceImpl implements TransactionService {
+    private AccountRepository accountRepository;
+    private TransactionRepository transactionRepository;
+
+    private static final Logger logger = LoggerFactory.getLogger(TransactionService.class);
+
+    @Override
+    @Transactional
+    public AccountResponse deposit(String accountNumber, BigDecimal amount) {
+        //finding the account
+        if(!accountRepository.existsByAccountNumber(accountNumber)) {
+            throw new CustomException.BadRequestException("No account found with the specified account number");
+        }
+        logger.info("Account Number: {}", accountNumber);
+        System.out.println(accountNumber);
+        Account account = accountRepository.findByAccountNumber(accountNumber);
+        logger.info("Account: {}", account);
+        //updating the account balance
+        account.setAccountBalance(account.getAccountBalance().add(amount));
+
+        Transaction transaction = Transaction.builder()
+                .toAccount(account)
+                .amount(amount)
+                .type(TransactionType.DEPOSIT)
+                .transactionDate(LocalDateTime.now())
+                .build();
+        transactionRepository.save(transaction);
+        //updating the account because its balance has changed
+        accountRepository.save(account);
+        return AccountResponse.builder()
+                .responseMessage("Transaction successful. Deposit of " + amount + " DH was added to account " + accountNumber)
+                .accountInfo(Account.builder()
+                        .accountNumber(account.getAccountNumber())
+                        .accountBalance(account.getAccountBalance())
+                        .build())
+                .build();
+    }
+
+    @Override
+    @Transactional
+    public AccountResponse withdrawal(String accountNumber, BigDecimal amount) {
+        //finding the account
+        if(!accountRepository.existsByAccountNumber(accountNumber)) {
+            throw new CustomException.BadRequestException("No account found with the specified account number");
+        }
+        Account account = accountRepository.findByAccountNumber(accountNumber);
+        // Checking if there is enough balance to withdraw
+        if (account.getAccountBalance().compareTo(amount) < 0) {
+            throw new CustomException.BadRequestException("Insufficient balance in account " + accountNumber);
+        }
+        //updating the account balance
+        account.setAccountBalance(account.getAccountBalance().subtract(amount));
+
+        Transaction transaction = Transaction.builder()
+                .toAccount(account)
+                .amount(amount)
+                .type(TransactionType.WITHDRAWAL)
+                .transactionDate(LocalDateTime.now())
+                .build();
+        transactionRepository.save(transaction);
+        //updating the account because its balance has changed
+        accountRepository.save(account);
+        return AccountResponse.builder()
+                .responseMessage("Transaction successful. Withdrawal of " + amount + " DH was deducted to account " + accountNumber)
+                .accountInfo(Account.builder()
+                        .accountNumber(account.getAccountNumber())
+                        .accountBalance(account.getAccountBalance())
+                        .build())
+                .build();
+    }
+
+    @Override
+    @Transactional
+    public TransferResponse transfer(TransferRequest transferRequest) {
+        //checking if both accounts exist
+        Account fromAccount = accountRepository.findByAccountNumber(transferRequest.fromAccountNumber());
+        Account toAccount = accountRepository.findByAccountNumber(transferRequest.toAccountNumber());
+
+        if (fromAccount == null) {
+            throw new CustomException.BadRequestException("Source account not found");
+        }
+        if (toAccount == null) {
+            throw new CustomException.BadRequestException("Destination account not found");
+        }
+
+        //checking if the source account has enough balance
+        if(fromAccount.getAccountBalance().compareTo(transferRequest.amount()) < 0) {
+            throw new CustomException.BadRequestException("Insufficient balance in the source account");
+        }
+
+        //transfering
+        fromAccount.setAccountBalance(fromAccount.getAccountBalance().subtract(transferRequest.amount()));
+        toAccount.setAccountBalance(toAccount.getAccountBalance().add(transferRequest.amount()));
+
+        accountRepository.save(fromAccount);
+        accountRepository.save(toAccount);
+
+        Transaction debitTransaction = Transaction.builder()
+                .toAccount(toAccount)
+                .fromAccount(fromAccount)
+                .amount(transferRequest.amount())
+                .type(TransactionType.TRANSFER)
+                .transactionDate(LocalDateTime.now())
+                .build();
+
+        Transaction creditTransaction = Transaction.builder()
+                .fromAccount(fromAccount)
+                .toAccount(toAccount)
+                .amount(transferRequest.amount())
+                .type(TransactionType.TRANSFER)
+                .transactionDate(LocalDateTime.now())
+                .build();
+
+        transactionRepository.save(debitTransaction);
+        transactionRepository.save(creditTransaction);
+
+        return TransferResponse.builder()
+                .responseMessage("Transaction successful")
+                .transactionInfo(Transaction.builder()
+                        .transactionDate(LocalDateTime.now())
+                        .type(TransactionType.TRANSFER)
+                        .build())
+                .build();
+    }
+
+    @Override
+    public List<TransactionHistoryResponse> getTransactionHistory(String accountNumber) {
+        Account account = accountRepository.findByAccountNumber(accountNumber);
+        if (account == null) {
+            throw new CustomException.BadRequestException("Account not found.");
+        }
+
+        List<Transaction> outgoingTransactions = transactionRepository.findByFromAccount(account);
+        List<Transaction> incomingTransactions = transactionRepository.findByToAccount(account);
+
+        //combining both lists
+        List<Transaction> transactions = new ArrayList<>();
+        transactions.addAll(outgoingTransactions);
+        transactions.addAll(incomingTransactions);
+
+        return  transactions.stream().map(transaction ->
+            TransactionHistoryResponse.builder()
+                    .amount(transaction.getAmount())
+                    .transactionDate(transaction.getTransactionDate())
+                    .type(transaction.getType())
+                    .recipientAccountNumber(transaction.getToAccount() != null ? transaction.getToAccount().getAccountNumber() : null)
+                    .build()
+        ).collect(Collectors.toList());
+    }
+}
