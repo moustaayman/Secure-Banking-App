9b3f50bec8ca97aa39dcf3983c85b7384aadefed
mousta <moustaayman03@gmail.com>
2024-11-02 18:48:15 +0100

    added transaction service

---
+++ src/main/java/com/ayman/bankapp/bankingapplication/services/TransactionService.java
@@ -0,0 +1,13 @@
+package com.ayman.bankapp.bankingapplication.services;
+
+import com.ayman.bankapp.bankingapplication.dtos.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface TransactionService {
+    AccountResponse deposit(String accountNumber, BigDecimal amount);
+    AccountResponse withdrawal(String accountNumber, BigDecimal amount);
+    TransferResponse transfer(TransferRequest transferRequest);
+    List<TransactionHistoryResponse> getTransactionHistory(String accountNumber);
+}
