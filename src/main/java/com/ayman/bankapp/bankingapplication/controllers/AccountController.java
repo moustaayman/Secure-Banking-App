842aca7357afea1c418808320d795252fc08e11a
mousta <moustaayman03@gmail.com>
2024-11-03 13:31:32 +0100

    added bank account statement download controller

--- src/main/java/com/ayman/bankapp/bankingapplication/controllers/AccountController.java
+++ src/main/java/com/ayman/bankapp/bankingapplication/controllers/AccountController.java
@@ -2,19 +2,26 @@ package com.ayman.bankapp.bankingapplication.controllers;
 
 import com.ayman.bankapp.bankingapplication.dtos.*;
 import com.ayman.bankapp.bankingapplication.services.AccountService;
+import com.ayman.bankapp.bankingapplication.services.BankStatementService;
 import com.ayman.bankapp.bankingapplication.services.TransactionService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @RestController
 @RequestMapping("/api/v1/accounts")
 @AllArgsConstructor
 public class AccountController {
     private AccountService accountService;
     private TransactionService transactionService;
+    private BankStatementService bankStatementService;
 
     @PostMapping(path = "/create")
     public ResponseEntity<AccountResponse> createAccount(@RequestBody AccountRequest accountRequest) {
@@ -32,6 +39,28 @@ public class AccountController {
     @PostMapping(path = "/withdrawal/{accountNumber}")
     public ResponseEntity<AccountResponse> withdrawal(@PathVariable String accountNumber, @RequestBody TransactionRequest transactionRequest) {
         return new ResponseEntity<>(transactionService.withdrawal(accountNumber, transactionRequest.amount()), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/{accountNumber}/statement")
+    public ResponseEntity<byte[]> downloadBankStatement(
+            @PathVariable String accountNumber,
+            @RequestParam("startDate") String startDate,
+            @RequestParam("endDate") String endDate) {
+
+        LocalDate start = LocalDate.parse(startDate);
+        LocalDate end = LocalDate.parse(endDate);
+
+        // Generate the PDF statement
+        byte[] pdfBytes = bankStatementService.generateBankStatement(accountNumber, start, end);
+
+        // Set headers to force download of PDF
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        headers.setContentDispositionFormData("attachment", "BankStatement_" + accountNumber + ".pdf");
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(pdfBytes);
     }
 
     @DeleteMapping(path = "/delete/{accountNumber}")
