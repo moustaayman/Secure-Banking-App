8643525a8a54b10ffa1925810c12ed8270068f22
mousta <moustaayman03@gmail.com>
2024-11-03 23:39:26 +0100

    modified sendMailWithAttachment stucture

--- src/main/java/com/ayman/bankapp/bankingapplication/services/EmailService.java
+++ src/main/java/com/ayman/bankapp/bankingapplication/services/EmailService.java
@@ -4,5 +4,5 @@ import com.ayman.bankapp.bankingapplication.dtos.EmailDetails;
 
 public interface EmailService {
     void sendSimpleMail(EmailDetails emailDetails);
-    void sendMailWithAttachment(EmailDetails emailDetails);
+    void sendEmailWithAttachment(String toEmail, String subject, String body, byte[] bankStatement, String pdfName);
 }
