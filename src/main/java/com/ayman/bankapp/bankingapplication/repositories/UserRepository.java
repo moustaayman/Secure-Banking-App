885806bfd2b800d6b4f1bb8ad2da65f2983ca812
mousta <moustaayman03@gmail.com>
2024-11-11 13:20:45 +0100

    minor

--- src/main/java/com/ayman/bankapp/bankingapplication/repositories/UserRepository.java
+++ src/main/java/com/ayman/bankapp/bankingapplication/repositories/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsById(String id);
     boolean existsByEmail(String email);
-    User findByEmail(String email);
+    User findUserById(String id);
 }
