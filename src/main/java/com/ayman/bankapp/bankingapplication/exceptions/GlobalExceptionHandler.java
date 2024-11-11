35440495f67181c438ae0168472231bf8942b75c
mousta <moustaayman03@gmail.com>
2024-11-11 13:24:13 +0100

    made the global exception handler generic

--- src/main/java/com/ayman/bankapp/bankingapplication/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/ayman/bankapp/bankingapplication/exceptions/GlobalExceptionHandler.java
@@ -7,35 +7,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(CustomException.NotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleNotFoundException(CustomException.NotFoundException ex) {
-        ErrorResponse errorResponse = new ErrorResponse("NOT_FOUND", ex.getMessage());
-        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ErrorResponse> handleCustomException(CustomException ex) {
+        HttpStatus status = ex.getStatus();
+        ErrorResponse errorResponse = new ErrorResponse(status.name(), ex.getMessage());
+        return new ResponseEntity<>(errorResponse, status);
     }
 
-    @ExceptionHandler(CustomException.BadRequestException.class)
-    public ResponseEntity<ErrorResponse> handleBadRequestException(CustomException.BadRequestException ex) {
-        ErrorResponse errorResponse = new ErrorResponse("BAD_REQUEST", ex.getMessage());
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(CustomException.UnauthenticatedException.class)
-    public ResponseEntity<ErrorResponse> handleUnauthenticatedException(CustomException.UnauthenticatedException ex) {
-        ErrorResponse errorResponse = new ErrorResponse("UNAUTHENTICATED", ex.getMessage());
-        return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
-    }
-
-    @ExceptionHandler(CustomException.UnauthorizedException.class)
-    public ResponseEntity<ErrorResponse> handleUnauthorizedException(CustomException.UnauthorizedException ex) {
-        ErrorResponse errorResponse = new ErrorResponse("UNAUTHORIZED", ex.getMessage());
-        return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
-    }
-
-    //generic exceptions
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGenericException(Exception ex) {
         ErrorResponse errorResponse = new ErrorResponse("INTERNAL_SERVER_ERROR", "An unexpected error occurred");
-        System.out.println(ex);
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
