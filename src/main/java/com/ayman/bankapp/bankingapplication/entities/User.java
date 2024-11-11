23bc5a74c96f7e1f97f8dbdb5328456baec64540
mousta <moustaayman03@gmail.com>
2024-11-11 13:21:39 +0100

    changed the user id logic generation

--- src/main/java/com/ayman/bankapp/bankingapplication/entities/User.java
+++ src/main/java/com/ayman/bankapp/bankingapplication/entities/User.java
@@ -12,14 +12,15 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 @Entity
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Table(name = "users")
 public class User {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Id
+    private String id;
     private String firstName;
     private String lastName;
     private String email;
@@ -37,6 +38,12 @@ public class User {
     private LocalDateTime registrationDate;
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Account> accounts = new ArrayList<>();
+    @PrePersist
+    public void generateId() {
+        if (this.id == null) {
+            this.id = UUID.randomUUID().toString();
+        }
+    }
     //is KYC verified ?
     //private boolean isKycVerified;
 }
