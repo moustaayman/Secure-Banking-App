c8e556a74edbb977ebde412ed7d510eca56cc7ff
mousta <moustaayman03@gmail.com>
2024-11-11 13:21:13 +0100

    added getUserAccounts controller

--- src/main/java/com/ayman/bankapp/bankingapplication/controllers/UserController.java
+++ src/main/java/com/ayman/bankapp/bankingapplication/controllers/UserController.java
@@ -2,11 +2,14 @@ package com.ayman.bankapp.bankingapplication.controllers;
 
 import com.ayman.bankapp.bankingapplication.dtos.UserRegistrationRequest;
 import com.ayman.bankapp.bankingapplication.dtos.UserResponse;
+import com.ayman.bankapp.bankingapplication.entities.Account;
 import com.ayman.bankapp.bankingapplication.services.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -17,5 +20,9 @@ public class UserController {
     @PostMapping(path = "/register")
     public ResponseEntity<UserResponse> registerUser(@RequestBody UserRegistrationRequest userRegistrationRequest) {
         return new ResponseEntity<>(userService.registerUser(userRegistrationRequest), HttpStatus.CREATED);
+    }
+    @GetMapping("/{userId}/accounts")
+    public ResponseEntity<List<Account>> getUserAccounts(@PathVariable String userId) {
+        return new ResponseEntity<>(userService.getUserAccounts(userId), HttpStatus.OK);
     }
 }
