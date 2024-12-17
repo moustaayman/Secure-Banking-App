fdcbb0a7806442c1630cfccceac484b8d52e6dbf
mousta <moustaayman03@gmail.com>
2024-12-17 22:09:01 +0100

    added methods

--- src/main/java/com/ayman/bankapp/bankingapplication/repositories/UserRepository.java
+++ src/main/java/com/ayman/bankapp/bankingapplication/repositories/UserRepository.java
@@ -4,9 +4,15 @@ import com.ayman.bankapp.bankingapplication.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.awt.print.Pageable;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsById(String id);
     boolean existsByEmail(String email);
     User findUserById(String id);
+    Optional<User> findUserByEmail(String email);
+    List<User> findAll();
 }
